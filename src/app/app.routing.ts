ae4e8728265a26f93bf59b1991eb75236e7ba87a
Talita Oliveira <litaa.oliveira@gmail.com>
2017-07-06 23:08:32 -0300

    ajuste rota

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,6 +7,7 @@ import { PerfilComponent } from './perfil/perfil.component';
 import { CardapioComponent } from './cardapio/cardapio.component';
 
 const APP_ROUTES: Routes = [
+	{ path: 'home', component: HomeComponent },
 	{ path: 'cardapio', component: CardapioComponent },
 	{ path: 'perfil', component: PerfilComponent },
 	{ path: 'pedidos', component: PedidosComponent },
