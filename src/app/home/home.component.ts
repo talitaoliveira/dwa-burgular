4dc740f7d50069821ec0773bd000dee78d96f22e
Talita Oliveira <litaa.oliveira@gmail.com>
2017-07-15 09:35:16 -0300

    ajuste na home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from './login/auth.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  usuarioLogado;
+
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+
+    this.usuarioLogado = this.authService.usuarioEstaAutenticado();
+
   }
 
 }
